da83febe8e99c7c8fb01fe69269f36dac344a1d4
jtuu <joona.tuukkanen@gmail.com>
2025-02-19 00:04:19 +0200

    Nicer styling for stat sorting options

--- StatFilter.js
+++ StatFilter.js
@@ -3,17 +3,18 @@ export const StatFilter = {
         const { filter_state } = vnode.attrs;
         return m("div.stat-sorting",
             m("div.stat-sum-sort", m("div", m("strong", "Sort by stat sum")),
-                Object.keys(filter_state.stat_sorting_options).map(stat_name => {
-                    return m("label", { key: stat_name }, [
-                        m("span", stat_name),
-                        m("input", {
-                            type: "checkbox",
-                            checked: filter_state.stat_sorting_options[stat_name],
-                            onchange: e => {
-                                filter_state.stat_sorting_options[stat_name] = e.target.checked;
-                            }
-                        })]);
-                })),
+                m("div.stat-sort-checkboxes",
+                    Object.keys(filter_state.stat_sorting_options).map(stat_name => {
+                        return m("label", { key: stat_name }, [
+                            m("input", {
+                                type: "checkbox",
+                                checked: filter_state.stat_sorting_options[stat_name],
+                                onchange: e => {
+                                    filter_state.stat_sorting_options[stat_name] = e.target.checked;
+                                }
+                            }),
+                            m("span", stat_name),]);
+                    }))),
             m("div.stat-min-filter", m("div", m("strong", "Minimum stats")),
                 Object.keys(filter_state.stat_minimum_filter).map(stat_name => {
                     return m("label", { key: stat_name },
